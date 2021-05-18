5df3fd7bcceec27f0e448712e0022f0fb45e663d
szuboy <szujeremy@gmail.com>
2021-05-18 21:39:34 +0800

    rename nnunet-paddle to paddlennUNet

---
+++ paddlennUNet/utilities/file_and_folder_operations.py
@@ -0,0 +1,90 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: jeremy.zhang(szujeremy@gmail.com, Shenzhen University, China)
+
+# link: https://github.com/MIC-DKFZ/batchgenerators/blob/master/batchgenerators/utilities/file_and_folder_operations.py
+# Since file_and_folder_operations.py is designed for Linux or MacOS,
+# in order to be compatible with Windows, we rewrite it for compatibility.
+# Don't worry! Usage and functional APIs are identical.
+
+
+import os
+import pickle
+import json
+
+
+def subdirs(folder, join=True, prefix=None, suffix=None, sort=True):
+    if join:
+        l = os.path.join
+    else:
+        l = lambda x, y: y
+    res = [l(folder, i) for i in os.listdir(folder) if os.path.isdir(os.path.join(folder, i))
+           and (prefix is None or i.startswith(prefix))
+           and (suffix is None or i.endswith(suffix))]
+    if sort:
+        res.sort()
+    return res
+
+
+def subfiles(folder, join=True, prefix=None, suffix=None, sort=True):
+    if join:
+        l = os.path.join
+    else:
+        l = lambda x, y: y
+    res = [l(folder, i) for i in os.listdir(folder) if os.path.isfile(os.path.join(folder, i))
+           and (prefix is None or i.startswith(prefix))
+           and (suffix is None or i.endswith(suffix))]
+    if sort:
+        res.sort()
+    return res
+
+
+subfolders = subdirs
+
+
+def maybe_mkdir_p(directory):
+    directory = os.path.abspath(directory)
+    if os.path.isdir(directory):
+        try:
+            os.makedirs(directory)
+        except FileExistsError:
+            print("WARNING: Folder %s already existed and does not need to be created" % directory)
+
+
+def load_pickle(file, mode='rb'):
+    with open(file, mode) as f:
+        a = pickle.load(f)
+    return a
+
+
+def write_pickle(obj, file, mode='wb'):
+    with open(file, mode) as f:
+        pickle.dump(obj, f)
+
+
+save_pickle = write_pickle
+
+
+def load_json(file):
+    with open(file, 'r') as f:
+        a = json.load(f)
+    return a
+
+
+def save_json(obj, file, indent=4, sort_keys=True):
+    with open(file, 'w') as f:
+        json.dump(obj, f, sort_keys=sort_keys, indent=indent)
+
+
+write_json = save_json
+
+
+def pardir(path):
+    return os.path.join(path, os.pardir)
+
+
+# I'm tired of typing these out
+join = os.path.join
+isdir = os.path.isdir
+isfile = os.path.isfile
+listdir = os.listdir
